c7ea5f794c64ce28c6df21876921d8b737078ad5
fauzanjaisyu <fauzanjaisyu@gmail.com>
2024-04-17 19:47:03 +0700

    Update welcome messages

--- epicarcade-frontend/src/App.js
+++ epicarcade-frontend/src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World! This is the Epic Arcade Frontend.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
