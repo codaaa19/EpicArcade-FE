13f2591f425f6a347db6333c47c4fb9213d64857
Tsabit Coda Rafisukmawan <tsabitiphone@gmail.com>
2024-05-26 21:14:03 +0700

    p

--- epicarcade-frontend/next.config.js
+++ epicarcade-frontend/next.config.js
@@ -1,12 +1,12 @@
-// next.config.js
-const path = require('path');
+// // next.config.js
+// const path = require('path');
 
-module.exports = {
-  reactStrictMode: true,
-  distDir: 'build',
-  // Add this configuration to tell Next.js where to find your pages directory
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.join(__dirname, 'src');
-    return config;
-  }
-};
+// module.exports = {
+//   reactStrictMode: true,
+//   distDir: 'build',
+//   // Add this configuration to tell Next.js where to find your pages directory
+//   webpack: (config) => {
+//     config.resolve.alias['@'] = path.join(__dirname, 'src');
+//     return config;
+//   }
+// };
